3aebd61ddd523992db1b5a54ee4ac20432178bad
Ashley <ashleypak71@yahoo.com>
2022-04-03 19:01:53 +0900

    Added model structure from MN3 paper as a dictionary
    - Tables 1 and 2 in the MN3 paper shows specifications for MN3-Small and
      MN3-Large
    - module_defs will be used in the _build_model method of MobileNetV3 to build
      the model
    - 'small' or 'large' will be used as an argument to model_size to select the
      correct architecture from the dictionary.

--- model.py
+++ model.py
@@ -2,6 +2,38 @@ from torch import nn
 import torch.nn.functional as F
 
 
+module_defs = {
+    # Table 1. and 2.
+    # kernel, exp size, out channels, squeeze excite, nonlinearities, stride
+    'large': [{'k': 3, 'exp': 16, 'oc': 16, 'se': False, 'act': 'RE', 's': 1},
+              {'k': 3, 'exp': 64, 'oc': 24, 'se': False, 'act': 'RE', 's': 2},
+              {'k': 3, 'exp': 72, 'oc': 24, 'se': False, 'act': 'RE', 's': 1},
+              {'k': 5, 'exp': 72, 'oc': 40, 'se': True, 'act': 'RE', 's': 2},
+              {'k': 5, 'exp': 120, 'oc': 40, 'se': True, 'act': 'RE', 's': 1},
+              {'k': 5, 'exp': 120, 'oc': 40, 'se': True, 'act': 'RE', 's': 1},
+              {'k': 3, 'exp': 240, 'oc': 80, 'se': False, 'act': 'HS', 's': 2},
+              {'k': 3, 'exp': 200, 'oc': 80, 'se': False, 'act': 'HS', 's': 1},
+              {'k': 3, 'exp': 184, 'oc': 80, 'se': False, 'act': 'HS', 's': 1},
+              {'k': 3, 'exp': 184, 'oc': 80, 'se': False, 'act': 'HS', 's': 1},
+              {'k': 3, 'exp': 480, 'oc': 112, 'se': True, 'act': 'HS', 's': 1},
+              {'k': 3, 'exp': 672, 'oc': 112, 'se': True, 'act': 'HS', 's': 1},
+              {'k': 5, 'exp': 672, 'oc': 160, 'se': True, 'act': 'HS', 's': 2},
+              {'k': 5, 'exp': 960, 'oc': 160, 'se': True, 'act': 'HS', 's': 1},
+              {'k': 5, 'exp': 960, 'oc': 160, 'se': True, 'act': 'HS', 's': 1}],
+    'small': [{'k': 3, 'exp': 16, 'oc': 16, 'se': True, 'act': 'RE', 's': 2},
+              {'k': 3, 'exp': 72, 'oc': 24, 'se': False, 'act': 'RE', 's': 2},
+              {'k': 3, 'exp': 88, 'oc': 24, 'se': False, 'act': 'RE', 's': 1},
+              {'k': 5, 'exp': 96, 'oc': 40, 'se': True, 'act': 'HS', 's': 2},
+              {'k': 5, 'exp': 240, 'oc': 40, 'se': True, 'act': 'HS', 's': 1},
+              {'k': 5, 'exp': 240, 'oc': 240, 'se': True, 'act': 'HS', 's': 1},
+              {'k': 5, 'exp': 120, 'oc': 48, 'se': True, 'act': 'HS', 's': 1},
+              {'k': 5, 'exp': 144, 'oc': 48, 'se': True, 'act': 'HS', 's': 1},
+              {'k': 5, 'exp': 288, 'oc': 96, 'se': True, 'act': 'HS', 's': 2},
+              {'k': 5, 'exp': 576, 'oc': 96, 'se': True, 'act': 'HS', 's': 1},
+              {'k': 5, 'exp': 576, 'oc': 96, 'se': True, 'act': 'HS', 's': 1}]
+}
+
+
 class HardSwish(nn.Module):  # 5.2 Nonlinearities
     def __init__(self, inplace=False):
         super(HardSwish, self).__init__()
@@ -45,8 +77,25 @@ class InvertedBottleNeck(nn.Module):  # TODO
 
 
 class MobileNetV3(nn.Module):  # TODO
-    def __init__(self):
+    def __init__(self, model_size: str):
+        assert model_size == 'small' or model_size == 'large',\
+            "model_size should be 'small' or 'large'."
+
         super(MobileNetV3, self).__init__()
+
+        if model_size == 'small':
+            last_in = 576
+            last_out = 1024
+        elif model_size == 'large':
+            last_in = 960
+            last_out = 1280
+
+        self.model = self._build_model(model_size)
 
     def forward(self, x):
         return
+
+    def _build_model(self, model_size:str) -> nn.Sequential:
+        modules = nn.Sequential()
+
+        return modules
