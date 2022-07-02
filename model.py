d5c82592063f59ceae26de5f83cd35d6a3bd05c6
Ashley <ashleypak71@yahoo.com>
2022-07-02 20:36:03 +0900

    Reformatted with Black

--- model.py
+++ model.py
@@ -8,34 +8,42 @@ import torch.nn.functional as F
 module_defs = {
     # Table 1. and 2.
     # kernel, exp size, out channels, squeeze excite, nonlinearities, stride
-    'large': [{'k': 3, 'exp': 16, 'oc': 16, 'se': False, 'act': 'RE', 's': 1},
-              {'k': 3, 'exp': 64, 'oc': 24, 'se': False, 'act': 'RE', 's': 2},
-              {'k': 3, 'exp': 72, 'oc': 24, 'se': False, 'act': 'RE', 's': 1},
-              {'k': 5, 'exp': 72, 'oc': 40, 'se': True, 'act': 'RE', 's': 2},
-              {'k': 5, 'exp': 120, 'oc': 40, 'se': True, 'act': 'RE', 's': 1},
-              {'k': 5, 'exp': 120, 'oc': 40, 'se': True, 'act': 'RE', 's': 1},
-              {'k': 3, 'exp': 240, 'oc': 80, 'se': False, 'act': 'HS', 's': 2},
-              {'k': 3, 'exp': 200, 'oc': 80, 'se': False, 'act': 'HS', 's': 1},
-              {'k': 3, 'exp': 184, 'oc': 80, 'se': False, 'act': 'HS', 's': 1},
-              {'k': 3, 'exp': 184, 'oc': 80, 'se': False, 'act': 'HS', 's': 1},
-              {'k': 3, 'exp': 480, 'oc': 112, 'se': True, 'act': 'HS', 's': 1},
-              {'k': 3, 'exp': 672, 'oc': 112, 'se': True, 'act': 'HS', 's': 1},
-              {'k': 5, 'exp': 672, 'oc': 160, 'se': True, 'act': 'HS', 's': 2},
-              {'k': 5, 'exp': 960, 'oc': 160, 'se': True, 'act': 'HS', 's': 1},
-              {'k': 5, 'exp': 960, 'oc': 160, 'se': True, 'act': 'HS', 's': 1}],
-    'small': [{'k': 3, 'exp': 16, 'oc': 16, 'se': True, 'act': 'RE', 's': 2},
-              {'k': 3, 'exp': 72, 'oc': 24, 'se': False, 'act': 'RE', 's': 2},
-              {'k': 3, 'exp': 88, 'oc': 24, 'se': False, 'act': 'RE', 's': 1},
-              {'k': 5, 'exp': 96, 'oc': 40, 'se': True, 'act': 'HS', 's': 2},
-              {'k': 5, 'exp': 240, 'oc': 40, 'se': True, 'act': 'HS', 's': 1},
-              {'k': 5, 'exp': 240, 'oc': 240, 'se': True, 'act': 'HS', 's': 1},
-              {'k': 5, 'exp': 120, 'oc': 48, 'se': True, 'act': 'HS', 's': 1},
-              {'k': 5, 'exp': 144, 'oc': 48, 'se': True, 'act': 'HS', 's': 1},
-              {'k': 5, 'exp': 288, 'oc': 96, 'se': True, 'act': 'HS', 's': 2},
-              {'k': 5, 'exp': 576, 'oc': 96, 'se': True, 'act': 'HS', 's': 1},
-              {'k': 5, 'exp': 576, 'oc': 96, 'se': True, 'act': 'HS', 's': 1}]
+    "large": [
+        {"k": 3, "exp": 16, "oc": 16, "se": False, "act": "RE", "s": 1},
+        {"k": 3, "exp": 64, "oc": 24, "se": False, "act": "RE", "s": 2},
+        {"k": 3, "exp": 72, "oc": 24, "se": False, "act": "RE", "s": 1},
+        {"k": 5, "exp": 72, "oc": 40, "se": True, "act": "RE", "s": 2},
+        {"k": 5, "exp": 120, "oc": 40, "se": True, "act": "RE", "s": 1},
+        {"k": 5, "exp": 120, "oc": 40, "se": True, "act": "RE", "s": 1},
+        {"k": 3, "exp": 240, "oc": 80, "se": False, "act": "HS", "s": 2},
+        {"k": 3, "exp": 200, "oc": 80, "se": False, "act": "HS", "s": 1},
+        {"k": 3, "exp": 184, "oc": 80, "se": False, "act": "HS", "s": 1},
+        {"k": 3, "exp": 184, "oc": 80, "se": False, "act": "HS", "s": 1},
+        {"k": 3, "exp": 480, "oc": 112, "se": True, "act": "HS", "s": 1},
+        {"k": 3, "exp": 672, "oc": 112, "se": True, "act": "HS", "s": 1},
+        {"k": 5, "exp": 672, "oc": 160, "se": True, "act": "HS", "s": 2},
+        {"k": 5, "exp": 960, "oc": 160, "se": True, "act": "HS", "s": 1},
+        {"k": 5, "exp": 960, "oc": 160, "se": True, "act": "HS", "s": 1},
+    ],
+    "small": [
+        {"k": 3, "exp": 16, "oc": 16, "se": True, "act": "RE", "s": 2},
+        {"k": 3, "exp": 72, "oc": 24, "se": False, "act": "RE", "s": 2},
+        {"k": 3, "exp": 88, "oc": 24, "se": False, "act": "RE", "s": 1},
+        {"k": 5, "exp": 96, "oc": 40, "se": True, "act": "HS", "s": 2},
+        {"k": 5, "exp": 240, "oc": 40, "se": True, "act": "HS", "s": 1},
+        {"k": 5, "exp": 240, "oc": 240, "se": True, "act": "HS", "s": 1},
+        {"k": 5, "exp": 120, "oc": 48, "se": True, "act": "HS", "s": 1},
+        {"k": 5, "exp": 144, "oc": 48, "se": True, "act": "HS", "s": 1},
+        {"k": 5, "exp": 288, "oc": 96, "se": True, "act": "HS", "s": 2},
+        {"k": 5, "exp": 576, "oc": 96, "se": True, "act": "HS", "s": 1},
+        {"k": 5, "exp": 576, "oc": 96, "se": True, "act": "HS", "s": 1},
+    ],
 }
-initialisations = {'normal': nn.init.normal_, 'xavier': nn.init.xavier_normal_, 'kaiming': nn.init.kaiming_normal_}
+initialisations = {
+    "normal": nn.init.normal_,
+    "xavier": nn.init.xavier_normal_,
+    "kaiming": nn.init.kaiming_normal_,
+}
 
 
 def calc_pad(k: int) -> int:
@@ -48,7 +56,7 @@ class HardSwish(nn.Module):  # 5.2 Nonlinearities
         self.inplace = inplace
 
     def forward(self, x):
-        return F.relu6(x+3., inplace=self.inplace) * 1./6. * x
+        return F.relu6(x + 3.0, inplace=self.inplace) * 1.0 / 6.0 * x
 
 
 class HardSigmoid(nn.Module):  # 5.2 Nonlinearsities
@@ -57,17 +65,23 @@ class HardSigmoid(nn.Module):  # 5.2 Nonlinearsities
         self.inplace = inplace
 
     def forward(self, x):
-        return F.relu6(x+3., inplace=self.inplace) * 1./6.
+        return F.relu6(x + 3.0, inplace=self.inplace) * 1.0 / 6.0
 
 
 class SqueezeExcite(nn.Module):
-    def __init__(self, c, r=4, inplace=True):  # 5.3 Large squeeze-and-excite, reduce to 1/4
+    def __init__(
+        self, c, r=4, inplace=True
+    ):  # 5.3 Large squeeze-and-excite, reduce to 1/4
         super(SqueezeExcite, self).__init__()
-        self.squeeze = nn.AdaptiveAvgPool2d(1)  # ((N), C, output_size, output_size), reduces to ((N), C, 1, 1)
-        self.excite = nn.Sequential(nn.Linear(c, c//r, bias=False),
-                                    nn.ReLU(inplace),
-                                    nn.Linear(c//r, c, bias=False),
-                                    HardSigmoid(inplace))
+        self.squeeze = nn.AdaptiveAvgPool2d(
+            1
+        )  # ((N), C, output_size, output_size), reduces to ((N), C, 1, 1)
+        self.excite = nn.Sequential(
+            nn.Linear(c, c // r, bias=False),
+            nn.ReLU(inplace),
+            nn.Linear(c // r, c, bias=False),
+            HardSigmoid(inplace),
+        )
 
     def forward(self, x):
         bs, c, _, _ = x.shape
@@ -77,29 +91,33 @@ class SqueezeExcite(nn.Module):
 
 
 class InvertedBottleNeck(nn.Module):
-    def __init__(self,
-                 ic: int,
-                 oc: int,
-                 k: int,
-                 exp: int,
-                 s: int,
-                 se: bool,
-                 act: str,
-                 drop_rate: Optional[float] = None):
+    def __init__(
+        self,
+        ic: int,
+        oc: int,
+        k: int,
+        exp: int,
+        s: int,
+        se: bool,
+        act: str,
+        drop_rate: Optional[float] = None,
+    ):
         super(InvertedBottleNeck, self).__init__()
-        self.pw1 = nn.Sequential(nn.Conv2d(ic, exp, 1, bias=False),
-                                 nn.BatchNorm2d(exp))
-        self.dw = nn.Sequential(nn.Conv2d(exp, exp, k, stride=s, groups=exp, padding=calc_pad(k), bias=False),
-                                nn.BatchNorm2d(exp))
-        self.pw2 = nn.Sequential(nn.Conv2d(exp, oc, 1, bias=False),
-                                 nn.BatchNorm2d(oc))
+        self.pw1 = nn.Sequential(nn.Conv2d(ic, exp, 1, bias=False), nn.BatchNorm2d(exp))
+        self.dw = nn.Sequential(
+            nn.Conv2d(
+                exp, exp, k, stride=s, groups=exp, padding=calc_pad(k), bias=False
+            ),
+            nn.BatchNorm2d(exp),
+        )
+        self.pw2 = nn.Sequential(nn.Conv2d(exp, oc, 1, bias=False), nn.BatchNorm2d(oc))
         self.se = SqueezeExcite(exp) if se else None
         self.drop_out = nn.Dropout(drop_rate) if drop_rate else None
         self.residual_connection = True if ic == oc and s == 1 else False
 
-        if act == 'RE':
+        if act == "RE":
             self.act = nn.ReLU(inplace=True)
-        elif act == 'HS':
+        elif act == "HS":
             self.act = HardSwish(inplace=True)
         else:
             raise NotImplementedError
@@ -129,48 +147,53 @@ class Classifier(nn.Module):
     def __init__(self, head_type: str, last_in: int, last_out: int, n_classes: int):
         super(Classifier, self).__init__()
         self.head_type = head_type
-        if self.head_type == 'fc':
-            self.classification_head = nn.Sequential(nn.AdaptiveAvgPool2d(1),
-                                                     nn.Linear(last_out, n_classes),
-                                                     nn.Softmax(1))
-        elif self.head_type == 'conv':
-            self.classification_head = nn.Sequential(nn.AdaptiveAvgPool2d(1),
-                                                     nn.Conv2d(last_in, last_out, 1, stride=1),
-                                                     HardSwish(inplace=True),
-                                                     nn.Conv2d(last_out, n_classes, 1))
+        if self.head_type == "fc":
+            self.classification_head = nn.Sequential(
+                nn.AdaptiveAvgPool2d(1), nn.Linear(last_out, n_classes), nn.Softmax(1)
+            )
+        elif self.head_type == "conv":
+            self.classification_head = nn.Sequential(
+                nn.AdaptiveAvgPool2d(1),
+                nn.Conv2d(last_in, last_out, 1, stride=1),
+                HardSwish(inplace=True),
+                nn.Conv2d(last_out, n_classes, 1),
+            )
         else:
             raise NotImplementedError
 
     def forward(self, x):
-        x = x.mean(3).mean(2) if self.head_type == 'fc' else x
+        x = x.mean(3).mean(2) if self.head_type == "fc" else x
         y = self.classification_head(x)
 
-        if self.head_type == 'conv':
+        if self.head_type == "conv":
             return y.view(y.shape[0], -1)
         else:
             return y
 
 
 class MobileNetV3(nn.Module):  # TODO
-    def __init__(self,
-                 model_size: str,
-                 n_classes: int,
-                 head_type: str,
-                 initialisation: Optional[str] = 'normal',
-                 drop_rate: Optional[float] = None,
-                 alpha=1.0):
-        assert model_size == 'small' or model_size == 'large',\
-            "model_size should be 'small' or 'large'."
-        assert 0. < alpha <= 1., "Width multiplier (alpha) is a value (0, 1]"
+    def __init__(
+        self,
+        model_size: str,
+        n_classes: int,
+        head_type: str,
+        initialisation: Optional[str] = "normal",
+        drop_rate: Optional[float] = None,
+        alpha=1.0,
+    ):
+        assert (
+            model_size == "small" or model_size == "large"
+        ), "model_size should be 'small' or 'large'."
+        assert 0.0 < alpha <= 1.0, "Width multiplier (alpha) is a value (0, 1]"
 
         super(MobileNetV3, self).__init__()
 
-        if model_size == 'small':
-            last_in = self._divisible(576 * alpha) if alpha < 1. else 576
-            last_out = self._divisible(1024 * alpha) if alpha < 1. else 1024
-        elif model_size == 'large':
-            last_in = self._divisible(960 * alpha) if alpha < 1. else 960
-            last_out = self._divisible(1280 * alpha) if alpha < 1. else 1280
+        if model_size == "small":
+            last_in = self._divisible(576 * alpha) if alpha < 1.0 else 576
+            last_out = self._divisible(1024 * alpha) if alpha < 1.0 else 1024
+        elif model_size == "large":
+            last_in = self._divisible(960 * alpha) if alpha < 1.0 else 960
+            last_out = self._divisible(1280 * alpha) if alpha < 1.0 else 1280
 
         self.model = self._build_model(model_size, last_in, drop_rate, alpha)
         self.classifier = Classifier(head_type, last_in, last_out, n_classes)
@@ -181,22 +204,30 @@ class MobileNetV3(nn.Module):  # TODO
         y = self.classifier(y)
         return y
 
-    def _build_model(self, model_size: str, last_in: int, drop_rate: float, alpha: float) -> nn.Sequential:
+    def _build_model(
+        self, model_size: str, last_in: int, drop_rate: float, alpha: float
+    ) -> nn.Sequential:
         modules = nn.Sequential()
-        ic = self._divisible(16 * alpha) if alpha < 1. else 16
+        ic = self._divisible(16 * alpha) if alpha < 1.0 else 16
 
         # Build the first block.
-        block_0 = nn.Sequential(nn.Conv2d(3, ic, 3, stride=2, padding=calc_pad(3), bias=False),
-                                nn.BatchNorm2d(ic),
-                                HardSwish(inplace=True))
+        block_0 = nn.Sequential(
+            nn.Conv2d(3, ic, 3, stride=2, padding=calc_pad(3), bias=False),
+            nn.BatchNorm2d(ic),
+            HardSwish(inplace=True),
+        )
         modules.append(block_0)
 
         # Build the MobileNet bottleneck blocks.
         defs = module_defs[model_size]
         for bn in defs:
-            oc = self._divisible(bn['oc'] * alpha) if alpha < 1. else bn['oc']
-            exp = self._divisible(bn['exp'] * alpha) if alpha < 1. else bn['exp']
-            modules.append(InvertedBottleNeck(ic, oc, bn['k'], exp, bn['s'], bn['se'], bn['act'], drop_rate))
+            oc = self._divisible(bn["oc"] * alpha) if alpha < 1.0 else bn["oc"]
+            exp = self._divisible(bn["exp"] * alpha) if alpha < 1.0 else bn["exp"]
+            modules.append(
+                InvertedBottleNeck(
+                    ic, oc, bn["k"], exp, bn["s"], bn["se"], bn["act"], drop_rate
+                )
+            )
             ic = oc
 
         # Build the last few blocks.
@@ -222,13 +253,21 @@ class MobileNetV3(nn.Module):  # TODO
                     nn.init.zeros_(module.bias)
 
     def _divisible(self, n, divisible_by=4):
-        return int(math.ceil(n * 1. / divisible_by) * divisible_by)
+        return int(math.ceil(n * 1.0 / divisible_by) * divisible_by)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import torch
+
     torch.manual_seed(42)
-    model = MobileNetV3(model_size='small', n_classes=10, head_type='conv', initialisation='kaiming', drop_rate=0.8, alpha=0.8)
+    model = MobileNetV3(
+        model_size="small",
+        n_classes=10,
+        head_type="conv",
+        initialisation="kaiming",
+        drop_rate=0.8,
+        alpha=0.8,
+    )
     x = torch.randn(16, 3, 32, 32)
     results = model(x)
     print(results.argmax(1))
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import torch
 from torch.utils.data import DataLoader
+from torch import nn
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 
@@ -20,7 +21,9 @@ class EMA:
         for name, param in self.model.named_parameters():
             if param.requires_grad:
                 assert name in self.shadow
-                new_avg = (1. - self.decay_rate) * param.data + self.decay_rate * self.shadow[name]
+                new_avg = (
+                    1.0 - self.decay_rate
+                ) * param.data + self.decay_rate * self.shadow[name]
                 self.shadow[name] = new_avg.clone()
 
     def restore(self):
@@ -47,13 +50,13 @@ def calc_mean_sd(dataloader: DataLoader):
         n_batches += 1
 
     mean = ch_sum / n_batches
-    sd = (ch_sum_sqrt / n_batches - mean ** 2) ** 0.5
+    sd = (ch_sum_sqrt / n_batches - mean**2) ** 0.5
     return mean, sd
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     torch.manual_seed(42)
-    dataset = datasets.CIFAR100('data', train=True, download=True, transform=ToTensor())
+    dataset = datasets.CIFAR100("data", train=True, download=True, transform=ToTensor())
     dataloader = DataLoader(dataset, batch_size=128)
     mean, sd = calc_mean_sd(dataloader)
     print(f"mean: {mean}, sd: {sd}")
